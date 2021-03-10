ba175bcf6f9232b101bc78649b50b8ad29205670
Fezekile Gwayi <fezekile101@gmail.com>
2021-03-10 19:41:05 +0200

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,1 +1,42 @@
 
+-- Table: netflix_titles
+CREATE TABLE netflix_titles (
+    duration_minutes int  ,
+    duration_seasons int  ,
+    type varchar(50)  ,
+    title varchar(150)  ,
+    date_added date  ,
+    release_year bigint  ,
+    rating varchar(50)  ,
+    description nvarchar(max) ,
+    show_id number  NOT NULL,
+    
+);
+
+-- Table: netflix_titles_cast
+CREATE TABLE netflix_titles_cast (
+    cast varchar(150)  ,
+    show_id bigint  NOT NULL,
+    CONSTRAINT netflix_titles_cast_pk PRIMARY KEY  (show_id)
+);
+
+-- Table: netflix_titles_category
+CREATE TABLE netflix_titles_category (
+    listed_in varchar(250) ,
+    show_id bigint  NOT NULL,
+    CONSTRAINT netflix_titles_category_pk PRIMARY KEY  (show_id)
+);
+
+-- Table: netflix_titles_countries
+CREATE TABLE netflix_titles_countries (
+    country varchar(100),
+    show_id int  NOT NULL,
+    CONSTRAINT netflix_titles_countries_pk PRIMARY KEY  (show_id)
+);
+
+-- Table: netflix_titles_directors
+CREATE TABLE netflix_titles_directors (
+    director varchar(150) ,
+    show_id bigint  NOT NULL,
+    CONSTRAINT netflix_titles_directors_pk PRIMARY KEY  (show_id)
+);
